b9c7cffd28b203d2095074f9827e1905e22855de
MatamorosF3 <matamorosf3@gmail.com>
2015-08-10 02:22:23 -0600

    Prueba de Threads

--- simcar/simcar/Form1.Designer.vb
+++ simcar/simcar/Form1.Designer.vb
@@ -23,31 +23,65 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(44, 25)
+        Me.Label1.Location = New System.Drawing.Point(82, 88)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(39, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Label1"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(85, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(293, 41)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Add2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(293, 88)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.ClientSize = New System.Drawing.Size(621, 291)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
-        Dim coolcar As New car(1, 2, 3)
-        Console.WriteLine(coolcar.gasolina)
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
 End Class
--- simcar/simcar/Form1.vb
+++ simcar/simcar/Form1.vb
@@ -1,3 +1,39 @@
 ﻿Public Class Form1
+    Private myThread As System.Threading.Thread
+    Private myThread2 As System.Threading.Thread
+    Dim i As Integer
+    Dim i2 As Integer
 
+    Private Sub add()
+
+        Do Until i2 = 10000
+            i2 = i2 + 1
+
+            Label1.Text = i2
+            Me.Refresh()
+        Loop
+
+    End Sub
+    Private Sub add2()
+
+        Do Until i = 10000
+            i = i + 1
+            Label2.Text = i
+            Me.Refresh()
+        Loop
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        myThread = New System.Threading.Thread(AddressOf add)
+        myThread.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        myThread2 = New System.Threading.Thread(AddressOf add2)
+        myThread2.Start()
+    End Sub
 End Class
